a067f60fd3bce2c96912ba3aec09690abe1aa579
user <devra576@gmail.com>
2022-11-10 15:18:47 +0700

    commit 2

--- app/src/main/java/com/example/hellotoast/MainActivity.java
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.hellotoast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private int mCount = 0;
+    private TextView mShowCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.textView2);
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message,Toast.LENGTH_SHORT);
+
+        toast.show();
+    }
+
+    public void showCount(View view) {
+        mCount++;
+        if(mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
     }
 }
